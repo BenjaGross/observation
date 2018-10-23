67434f441ae9908ad62d3e01e1dea7c0aaa1bd2b
Benjamin Gross <benja.j.gross@gmail.com>
2018-10-23 13:59:22 -0400

    Rough draft done

--- presentation.rb
+++ presentation.rb
@@ -1,11 +1,71 @@
 require 'slide_hero'
 
-presentation 'observation' do
-  slide "Presentations made easy!" do
-    point "Make presentations at the speed of thought"
-    point "Make ideas pop", animation: "grow"
+presentation 'Active Experimentation Vs Passive Observation' do
+  slide "Passive Observation" do
+    remote_image "https://media.giphy.com/media/YMzA0FcpbQwkU/giphy.gif", as: "empire_state_building"
+    point "Andy Warhol's Empire"
+  end
+
+  slide "What is Passive Observation" do
+    point "The action of noticing the world around you"
+    point "To see an activity and take not of it"
+  end
+
+  slide "Observing" do
+    point "Gather as much information as possible to later correlate cause and effect"
+    point "Refrain from making judgments or coming up with solutionsThese will tint how you view later actions"
+    point "Subject checked twice vs. Subject couldn't rememeber"
+  end
+
+  slide "Heisenberg's Uncertainty Principle" do
+    point "Our observations have an effect on the behavior of quanta"
+    point "This happens on a non quantum scale too, when you observe someone or some process they act differently"
+  end
+
+  slide "After Observing" do
+    point "Organize your information"
+    point "Look for trends"
+    point "Beign to look for solutions to problems"
+  end
+
+  slide "How to mitigate your presence" do
     list do
-      point "All kinds of awesome here"
+      point "Stay quiet"
+      point "Stay out of the way"
+      point "Focus your observations, you can't notice everything at once"
+    end
+    point "Stay as unobstrusive as posible"
+  end
+
+  slide "Active Experiment" do
+    remote_image "http://artdiscovery.info/wp-content/uploads/2013/07/4-IMG_1902.jpg", as: "maralyn"
+    point "Andy Warhol's Marilyn Diptych"
+  end
+
+  slide "What is Active Experimenting" do
+    point "Exactly what it sounds like"
+    point "Take an active role in actions"
+    point "Takes steps to affect the outcome"
+    point "See how your actions changed things"
+  end
+
+
+  slide "When do Obvserve vs Experiment" do
+    point "Observe"
+    list do
+      point "You are unfamiliar"
+      point "You don't know the problem"
+      point "You are too familiar"
+    end
+  end
+
+  slide "When to Observe vs Experiment" do
+    point "Experiment"
+    list do
+      point "You know your issue"
+      point "You know what you want to change"
+      point "You have a way "
+      point "You have a way to measure the effectiveness"
     end
   end
 end
